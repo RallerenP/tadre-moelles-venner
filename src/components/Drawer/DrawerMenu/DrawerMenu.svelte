aa654809d4bdbe618cf3e8d25140e8e19d2e6828
Rasmus Povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-02-25 20:52:30 +0100

    close #7 design drawer menu

--- src/components/Drawer/DrawerMenu/DrawerMenu.svelte
+++ src/components/Drawer/DrawerMenu/DrawerMenu.svelte
@@ -1,13 +1,14 @@
 <script>
     import {getDummyTree} from '../../../models/Tree/Tree';
     import DrawerMenuItem from "./MenuItem/DrawerMenuItem.svelte";
+    import { flip } from 'svelte/animate';
 
     let tree = getDummyTree();
 </script>
 
 <div class="h-full w-full content flex flex-col items-end justify-start">
-    {#each tree.items as item}
-        <DrawerMenuItem {item}/>
+    {#each tree.items as item, index (item)}
+        <DrawerMenuItem  {item}/>
     {/each}
 </div>
 
--- src/components/Drawer/DrawerMenu/MenuItem/DrawerMenuItem.svelte
+++ src/components/Drawer/DrawerMenu/MenuItem/DrawerMenuItem.svelte
@@ -1,14 +1,24 @@
 <script>
     import DrawerMenuItem from "./DrawerMenuItem.svelte";
+    import { fade } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
     export let item;
+
+    let open = false;
+
+    const handleToggle = () => {
+        open = !open;
+    }
 </script>
 
-<div class="h-16 flex " style={`width: calc(100% - ${item.depth * 48}px)`}>
-    <a class="underline hover:text-gray-600 cursor-pointer pl-8 flex-left w-full text-xl">{item.name}</a>
+<div transition:fade={{duration: 150}} class="h-16 flex border-primary" style={`width: calc(100% - ${item.depth * 48}px)`}>
+    <a class="underline hover:text-gray-600 cursor-pointer pl-8 flex-left w-full text-xl" href={item.url}>{item.name}</a>
     {#if item.items.length !== 0}
-        <div class="bg-white z-auto h-16 w-16 flex-shrink-0 flex-center text-2xl hover:bg-gray-100 cursor-pointer">+</div>
+        <div class="bg-white z-auto h-16 w-16 flex-shrink-0 flex-center text-2xl hover:bg-gray-100 cursor-pointer" on:click={handleToggle}>{open ? '-' : '+'}</div>
     {/if}
 </div>
-{#each item.items as item}
-    <DrawerMenuItem {item} />
-{/each}
+{#if open}
+    {#each item.items as item}
+        <DrawerMenuItem  {item} />
+    {/each}
+{/if}
