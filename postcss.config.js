a0d00d759a1fc2a00952adbdd03cfeff39694d79
Rasmus Povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-02-25 22:50:47 +0100

    Fixed PurgeCSS, again

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,23 @@
-const plugins =
-  process.env.NODE_ENV === 'production'
-    ? ['tailwindcss', 'autoprefixer', '@fullhuman/postcss-purgecss']
-    : ['tailwindcss'];
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
 
-module.exports = { plugins };
+    // Specify the paths to all of the template files in your project
+    content: [
+        './src/**/*.html',
+        './src/**/*.svelte',
+        // etc.
+    ],
 
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
+    ]
+}
