2f30f11fc4c0f02a131e37ca8d257eb4f223c990
Rasmus Povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-02-25 22:41:58 +0100

    Fixed PurgeCSS Config

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,7 +1,8 @@
-const plugins =
-  process.env.NODE_ENV === 'production'
-    ? ['tailwindcss', 'autoprefixer', '@fullhuman/postcss-purgecss']
-    : ['tailwindcss'];
-
-module.exports = { plugins };
-
+module.exports = {
+    content: [
+        './src/index.html',
+        './src/**/*.svelte'
+    ],
+    whitelistPatterns: [/svelte-/],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+};
