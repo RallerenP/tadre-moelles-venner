249fe432e7b93cc44762c3f32be97192eac7ed1f
Rasmus Povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-02-26 17:36:56 +0100

    close #23 implement pdf viewer

---
+++ src/components/PDFViewer/PFDViewer.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf'
+</script>
+
+<div class="bg-green-400 w-full h-64">
+    <embed src={url} type="application/pdf" width="100%" height="100%">
+</div>
